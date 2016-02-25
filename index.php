d690f01ef6227eb9b1268e2da2d9795e99ea811b
David Bretz <david.slimcognito@gmail.com>
2016-02-24 18:50:51 -0900

    Turned index into a landing page where you can navigate to different parts of the site. Currently, you can go to FormsPage.php and VolunteerRegistration.php from index.php. FormsPage.php has requests and displays the user's coordinates. VolunteerRegistration.php is currently a placeholder.

--- index.php
+++ index.php
@@ -7,12 +7,26 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>OCC Sign-In</title>
     </head>
-    <body>
-        <?php
-        // put your code here
-        echo "<p>Hello world </p>";
-        ?>
-    </body>
+    
+   <p>Where to buddy?
+   <p><a href="FormsPage.php">Fill out form</a></p>
+   <p><a href="VolunteerRegistration.php">Short-Term Volunteers</a></p>
+    
+<!--    <p><a href="">Insert Person?</a>
+    <p>Retrieve Person?
+    <p><a href="RetrievedOrganization.php">Retrieve Organization?
+    </a>
+    <p><a href="BoggleBoard.php">Boggle Game</a>-->
+
+    <?php
+    $lat = 61.19824389999999;
+    $lon = -149.7488146;
+    ?>
+
+</body>
 </html>
+
+<?php ?>
+
