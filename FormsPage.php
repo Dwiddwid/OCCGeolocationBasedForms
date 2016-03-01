f7c724e60474cd014738f3380cc09aded2905a9f
David Bretz <david.slimcognito@gmail.com>
2016-03-01 09:18:18 -0900

    Removed unnecessary netbeans files, added a form to the forms page, and printed the webpages using php.
    Removed unnecessary netbeans files and added a form to the forms page. I
    also printed most of the html for the website using php to see if
    doxygen would document it better. OCCSQLI_Connect.php is the file that
    is used to connect to the database.

--- FormsPage.php
+++ FormsPage.php
@@ -9,35 +9,71 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title></title>
     </head>
-    <body>
-        <p>This is a test of geolocation. Your location will be displayed below.</p>
+    <?php
+    print'<body onload="getLocation()">';
 
-    <!--    <button onclick="getLocation()">Click for location</button>-->
-
-    <p id="demo"></p>
-
-    <div id="map"></div>
+    print '<p>This is a test of geolocation. Your location will be displayed below.</p>';
 
 
+    print '<p id="demo"></p>';
 
-    <script>
-        var x = document.getElementById("demo");
+    print '<div id="map"></div>';
 
-        window.onload = function getLocation() {
+
+
+    print '<script>';
+    print 'var x = document.getElementById("demo");';
+
+    print 'function getLocation() {
             if (navigator.geolocation) {
                 navigator.geolocation.getCurrentPosition(showPosition);
             } else {
                 x.innerHTML = "Geolocation is not supported by this browser.";
             }
-        }
+        }';
 
-        function showPosition(position) {
+    print 'function showPosition(position) {
             x.innerHTML = "Latitude: " + position.coords.latitude +
                     "<br>Longitude: " + position.coords.longitude;
+        }';
+    print '</script>';
+
+
+    print '</body>';
+
+
+    print '<form action="PeopleAdded.php" method="post">
+    First Name: <input type="text" name="first"><br>
+    Last Name: <input type="text" name="last"><br>
+    Suffix: <input name="suffix" type="text" size="5" maxlength="5"><br>
+    Street: <input type="text" name="street"><br>
+    City: <input type="text" name="city"><br>
+    Zip: <input name="zip" type="text" size="5" maxlength="5"><br>
+    State: <select name="State" size="1" id="State">
+    <option value="AK">Alaska</option>
+    </select><br>
+    Mailing: <input type="text" name="mailing"><br>
+    Phone: <input type="text" name="phone"><br>
+    Email: <input type="text" name="email"><br>
+    Church: <select name="church" size="1" id="church">
+      <option value="NULL" selected="selected"></option>';
+
+    require_once('OCCSQLI_Connect.php');
+
+    $query = 'SELECT officialName FROM organization WHERE organizationID = 1 ORDER BY officialName/;';
+
+    $response = mysqli_query($dbc, $query);
+
+    if ($response) {
+        while ($row = mysqli_fetch_array($response)) {
+            echo '<option value="' . $row['officialName'] . '">' . $row['officialName'] . '</option>';
         }
-    </script>
-        <?php
-        // put your code here
-        ?>
-    </body>
+    }
+    mysqli_close($dbc);
+
+    print '</select>';
+    print '<br>';
+    print '<input type="Submit">';
+    print '</form>';
+    ?>
 </html>
--- index.php
+++ index.php
@@ -9,20 +9,18 @@ and open the template in the editor.
         <meta charset="UTF-8">
         <title>OCC Sign-In</title>
     </head>
+   <?php 
+   print '<p>Where to buddy?';
+   print '<p><a href="FormsPage.php">Fill out form</a></p>';
+   print '<p><a href="VolunteerRegistration.php">Short-Term Volunteers</a></p>';
     
-   <p>Where to buddy?
-   <p><a href="FormsPage.php">Fill out form</a></p>
-   <p><a href="VolunteerRegistration.php">Short-Term Volunteers</a></p>
-    
-<!--    <p><a href="">Insert Person?</a>
-    <p>Retrieve Person?
-    <p><a href="RetrievedOrganization.php">Retrieve Organization?
-    </a>
-    <p><a href="BoggleBoard.php">Boggle Game</a>-->
+//<!--    <p><a href="">Insert Person?</a>
+//    <p>Retrieve Person?
+//    <p><a href="RetrievedOrganization.php">Retrieve Organization?
+//    </a>
+//    <p><a href="BoggleBoard.php">Boggle Game</a>-->
 
-    <?php
-    $lat = 61.19824389999999;
-    $lon = -149.7488146;
+
     ?>
 
 </body>
